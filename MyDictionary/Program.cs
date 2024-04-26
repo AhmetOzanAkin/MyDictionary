e21a9030cf0f0f8ea5c3a35c053edd85c0b7c864
AhmetOzanAkin <oznakin@outlook.com>
2024-04-26 12:42:12 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyDictionary<TKey, TValue>
+{
+    private readonly Dictionary<TKey, TValue> dictionary;
+
+    public MyDictionary()
+    {
+        dictionary = new Dictionary<TKey, TValue>();
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        if (dictionary.ContainsKey(key))
+        {
+            Console.WriteLine($"Anahtar '{key}' zaten var. Değer '{value}' eklenemedi.");
+        }
+        else
+        {
+            dictionary.Add(key, value);
+            Console.WriteLine($"Anahtar '{key}' ve değer '{value}' başarıyla eklendi.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyDictionary<string, int> dictionary = new MyDictionary<string, int>();
+        dictionary.Add("apple", 10);
+        dictionary.Add("banana", 20);
+        dictionary.Add("cherry", 30);
+    }
+}
